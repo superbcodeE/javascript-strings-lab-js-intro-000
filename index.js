188f05ae4730afb35c6bee28281f91e05c419d99
Eliseo Cutz <cutzeliseo@gmail.com>
2021-03-22 13:22:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting ="Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest= "" + specialGuest + "!";
+var greetSpecialGuest= "Hello," + specialGuest + "!";//"hello,neil degrasse Tyson!"
 
 var topic = "space";
 
