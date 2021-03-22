225b26efdcc8506b04108e35221ded8178f44948
Eliseo Cutz <cutzeliseo@gmail.com>
2021-03-22 13:38:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var greetSpecialGuest= "Hello," + specialGuest + "!";//"hello,neil degrasse Tyso
 
 var topic = "space";
 
-var conversation = `about,${space}! conversation{space}!`;//" talk about space!"
+var conversation = `about,${space}! about{space}!`;//" talk about space!"
